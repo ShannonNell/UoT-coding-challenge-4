6cc8524efa273161767682c44fd18b72fc51abb6
Shannon <shannon.nell92@gmail.com>
2021-05-10 18:51:38 -0400

    Loops through questions, displays if correct or not; no Question displayed, no gameOver

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,8 @@ var choice1 = document.getElementById('ch1');
 var choice2 = document.getElementById('ch2');
 var choice3 = document.getElementById('ch3');
 var choice4 = document.getElementById('ch4');
-
-
+//answer
+var checkAnsDiv = document.querySelector('.checkAnswer');
 
 
 //array of questions to loop through
@@ -23,49 +23,74 @@ var questions = [
         q1: 'Commonly used data types DO NOT include:', 
         a1: 'Strings',
         a2: 'Booleans',
-        a3: 'Alerts', 
+        a3: 'Alerts - CORRECT', 
         a4: 'Numbers',
-        correctAnswer: '3'
+        correctAnswer: 'ch3'
     },
     { 
         q2: 'The condition in an if/else statement is enclosed within _______.', 
         a1: 'Quotes',
         a2: 'Curly Brackets',
-        a3: 'Parentheses', 
+        a3: 'Parentheses - CORRECT', 
         a4: 'Square Brackets',
-        correctAnswer: '3'
+        correctAnswer: 'ch3'
     },
     { 
         q3: 'Arrays in JavaScript can be used to store _______.', 
         a1: 'Numbers and Strings',
         a2: 'Other Arrays',
         a3: 'Booleans', 
-        a4: 'All of the Above',
-        correctAnswer: '4'
+        a4: 'All of the Above - CORRECT',
+        correctAnswer: 'ch4'
     },
     { 
         q4: 'String values must be enclosed within _______ when being assigned to variables.', 
         a1: 'Commas',
         a2: 'Curly Brackets',
-        a3: 'Quotes', 
+        a3: 'Quotes - CORRECT', 
         a4: 'Parentheses',
-        correctAnswer: '3'
+        correctAnswer: 'ch3'
     },
     { 
         q5: 'A very useful tool used during development and debugging for printing content to the debugger is:', 
         a1: 'JavaScript',
         a2: 'Terminal/Bash',
         a3: 'For loops', 
-        a4: 'Console.log',
-        correctAnswer: '4'
+        a4: 'Console.log - CORRECT',
+        correctAnswer: 'ch4'
     },
 ];
+
 
 // global variables
 var timeInterval;
 var timeLeft = 75;
 var lastQuest = questions.length - 1;
 var currentQuest = 0;
+
+
+// start quiz function
+startBtn.addEventListener('click', function() {
+    //timer starts
+    timeInterval = setInterval(function() {
+        //if time left still
+        if(timeLeft > 0) {
+            timerEl.textContent = 'Time: ' + timeLeft;
+            timeLeft--; 
+        }
+        //if no time left
+        else {
+            timerEl.textContent = 'Time: ' + timeLeft;
+            clearInterval(timeInterval);
+            //high score/game over page
+        }    
+    }, 1000);
+    //question appears
+    mainDiv.style.display = 'none';
+    quiz.style.display = 'block';
+    //question function runs
+    quizQuestions();
+}); 
 
 
 // function to present questions
@@ -81,30 +106,11 @@ function quizQuestions() {
 };
 
 
-// timer countdown function
-startBtn.addEventListener('click', function() {
-    timeInterval = setInterval(function() {
-        if(timeLeft > 0) {
-            timerEl.textContent = 'Time: ' + timeLeft;
-            timeLeft--; 
-        }
-        else {
-            timerEl.textContent = 'Time: ' + timeLeft;
-            clearInterval(timeInterval);
-        }    
-    }, 1000);
-    //removes Main Div and displays question
-    mainDiv.style.display = 'none';
-    quiz.style.display = 'block';
-    quizQuestions();
-}); 
 
-
-// TODO: When answer question, presented with another question
-// TODO: If question answered wrong, time subtracted from clock
+// check answer
 function checkAnswer(answer) {
     //check if user answer matches correct answer
-    if(questions[currentQuest].correctAnswer == answer){
+    if(answer === questions[currentQuest].correctAnswer){
         answerIsCorrect();
     } else {
         answerIsWrong();
@@ -112,12 +118,22 @@ function checkAnswer(answer) {
     //if there are still more questions, go to next question
     if(currentQuest < lastQuest) {
         currentQuest++;
+        quizQuestions();
     //if there are no more questions, display end/ game over
     } else {
-        gameOver();
+        //gameOver();
     }
 }
 
+function answerIsCorrect() {
+    checkAnsDiv.textContent = 'Correct!';
+    checkAnsDiv.style.display = 'block';
+;}
+
+function answerIsWrong() {
+    checkAnsDiv.textContent = 'Wrong!';
+    checkAnsDiv.style.display = 'block';
+};
 
 // TODO: When timer at 0 or questions answered, game over
 //gameOver();
